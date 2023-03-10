16fc953cfea049baad59506338a1857f3c12a2c7
Aman Mavai <amanmavai@gmail.com>
2023-03-10 05:47:49 +0000

    update vite config to support lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as packageJson from "./package.json";
+
+const packageName = packageJson.name;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+  ],
+  build: {
+    lib: {
+      entry: `src/${packageName}.ts`,
+      formats: ["es"],
+      fileName: (format) => `${packageName}.mjs`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+    emptyOutDir: false,
+  },
 })
